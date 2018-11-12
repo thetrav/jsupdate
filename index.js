45851304c5c50de8762788df49584891da9aa8ea
Travis Dixon <the.trav@gmail.com>
2018-11-12 12:37:24 +1100

    implement

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+const array = /(\w+)\[(\d+)]/;
+
 function jsu(obj, path, fn) {
-  return {};
+  if(typeof path === 'string' || path instanceof String) {
+    return jsu(obj, parse(path), fn);
+  }
+  
+  if(path.length == 0) {
+    return fn(obj);
+  }
+  
+  const [head, ...tail] = path;
+  const arrayParts = head.match(array);
+  
+  if(arrayParts) {
+    const name = arrayParts[1];
+    const index = parseInt(arrayParts[2]);
+
+    const newArray = obj[name].map((v, i) => {
+      return (i === index) ? jsu(v, tail, fn) : v;
+    })
+
+    return {...obj, [name]: newArray};
+  } else {
+    return {...obj, [head]: jsu(obj[head], tail, fn)};
+  }
+}
+
+function parse(path) {
+  return path.split(".")
 }
 
 export default jsu
--- index.test.js
+++ index.test.js
@@ -1,6 +1,46 @@
-
 import jsu from "./index";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+const original = {
+  foo: "fooValue",
+  bars: [
+    { ork: "ork1Value", bif: "bif1Value" },
+    { ork: "ork2Value", bif: "bif2Value" }
+  ]
+};
+
+test('replace string', () => {
+  const updated = jsu(original, "foo", (_) => "sommick");
+  expect(original.foo).toEqual("fooValue");
+  expect(updated.foo).toEqual("sommick");
+})
+
+test('replace whole array', () => {
+  const updated = jsu(original, "bars", (_) => [1,2,3]);
+  expect(original.bars.length).toBe(2);
+  expect(updated.bars).toEqual([1,2,3]);
+})
+
+test("replace value in array", () => {
+  const updated = jsu(original, "bars[0]", (_) => {return {test: true}});
+  expect(original.bars).toEqual([
+    { ork: "ork1Value", bif: "bif1Value" },
+    { ork: "ork2Value", bif: "bif2Value" }
+  ]);
+  expect(updated.bars).toEqual([{test: true}, 
+    { ork: "ork2Value", bif: "bif2Value" }
+  ]);
+})
+
+test('replace nested value in array', () => {
+  const updated = jsu(original, "bars[0].ork", (_) => "newOrk1Value");
+  expect(updated).not.toBe(original);
+  expect(original.bars[0]).toEqual({ork: "ork1Value", bif: "bif1Value"});
+  expect(updated.bars[0]).toEqual({ork: "newOrk1Value", bif: "bif1Value"});
 });
+
+test('transform nested value in array', () => {
+  const updated = jsu(original, "bars[0].ork", (v) => v.toUpperCase());
+  expect(original.bars[0]).toEqual({ork: "ork1Value", bif: "bif1Value"});
+  expect(updated.bars[0]).toEqual({ork: "ORK1VALUE", bif: "bif1Value"});
+
+})
